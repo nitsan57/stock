ec7b687b1bdb3d6a111dcacbe6b1d1cc4d863491
nitsan57 <nitsan57@gmail.com>
2020-11-15 17:32:33 +0200

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,21 @@ class App extends React.Component {
     };
     this.updateMessage = this.updateMessage.bind(this);
     // this.read_csv();
+    this.Auth();
   }
+
+  Auth() {
+    // Simple POST request with a JSON body using fetch
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: 'React POST Request Example' })
+    };
+    fetch("https://openapigw.tase.co.il/tase/prod/oauth/oauth2/NjViMGRjZjNiN2VjNDIyMDgwYzIwMTU0MmI1ZWE0ODU6MzlmMzVlM2MyYmEzNmJiMjA3ODA3ODkxNjQ5N2ZkYTI=", requestOptions)
+        .then(response => response.json())
+        .then(data => console.log(data)
+        );
+}
 
     // read_csv(){
     //         //Papa.parse("https://cors-anywhere.herokuapp.com/https://www.globes.co.il/portal/instrument/instrumentgraph_toexcel.aspx?instrumentid=981&feeder=0", {
