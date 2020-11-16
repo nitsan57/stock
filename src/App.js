35f6d236a598df4bb8465cfbd96d5169e365d246
nitsan57 <nitsan57@gmail.com>
2020-11-16 09:32:39 +0200

    [Auth] get login token from tase

--- src/App.js
+++ src/App.js
@@ -9,28 +9,45 @@ class App extends React.Component {
         data : {x: [1,2,3,4], y : [1,2,3,4]}
     };
     this.updateMessage = this.updateMessage.bind(this);
+    this.auth();
+
     // this.read_csv();
-    this.Auth();
   }
 
-  Auth() {
-    // Simple POST request with a JSON body using fetch
+  
+
+  auth() {
+
+    let body_details = {
+      'grant_type': 'client_credentials',
+      'scope': 'tase'
+  };
+
+  let formBody = [];
+  for (let property in body_details) {
+      let encodedKey = encodeURIComponent(property);
+      let encodedValue = encodeURIComponent(body_details[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+  }
+  formBody = formBody.join("&");
+
     const requestOptions = {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title: 'React POST Request Example' })
+        headers: {
+        "Authorization":"Basic NjViMGRjZjNiN2VjNDIyMDgwYzIwMTU0MmI1ZWE0ODU6MzlmMzVlM2MyYmEzNmJiMjA3ODA3ODkxNjQ5N2ZkYTI",
+      "Content-Type": "application/x-www-form-urlencoded" },
+        body: formBody
     };
-    fetch("https://openapigw.tase.co.il/tase/prod/oauth/oauth2/NjViMGRjZjNiN2VjNDIyMDgwYzIwMTU0MmI1ZWE0ODU6MzlmMzVlM2MyYmEzNmJiMjA3ODA3ODkxNjQ5N2ZkYTI=", requestOptions)
+    fetch("https://openapigw.tase.co.il/tase/prod/oauth/oauth2/token",requestOptions)
         .then(response => response.json())
         .then(data => console.log(data)
         );
 }
 
+
     // read_csv(){
     //         //Papa.parse("https://cors-anywhere.herokuapp.com/https://www.globes.co.il/portal/instrument/instrumentgraph_toexcel.aspx?instrumentid=981&feeder=0", {
-    //   var reader = new FileReader();
 
-    //   reader.readAsText("/Data/1146430.csv");
     //   // var  x= new File(['foo', 'bar'], "/Data/1146430.csv");
       
     //   console.log(reader);
