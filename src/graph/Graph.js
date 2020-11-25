21347a25578a35d06a027e7d638a529454e66c15
nitsan57 <nitsan57@gmail.com>
2020-11-25 15:10:05 +0200

    [Search] gets right list of ids

--- src/graph/Graph.js
+++ src/graph/Graph.js
@@ -1,124 +1,130 @@
 import React from 'react';
 // import Plotly  from 'react-plotly.js';
 // import Plot from "react-plotly.js";
-import Plotly from "plotly.js-basic-dist";
+import Plotly from 'plotly.js-basic-dist';
 
-import createPlotlyComponent from "react-plotly.js/factory";
+import createPlotlyComponent from 'react-plotly.js/factory';
 const Plot = createPlotlyComponent(Plotly);
 
 class Graph extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [],
+			is_data_loaded: false,
+		};
+		this.create_graph_data = this.create_graph_data.bind(this);
+		this.setStateAsync = this.setStateAsync.bind(this);
+	}
 
-  constructor(props){  
-    super(props);  
-    this.state = {  
-        data: [],
-        is_data_loaded : false,
-    };
-    this.create_graph_data = this.create_graph_data.bind(this);
+	create_graph_data(x, y, name) {
+		return {
+			x: x,
+			y: y,
+			type: 'scatter',
+			mode: 'lines+markers',
+			name: name,
+		};
+	}
 
-  }
+	componentDidMount() {}
 
-  create_graph_data(x, y, name){
-    return {
-      "x": x,
-      "y": y,
-      type: 'scatter',
-      mode: 'lines+markers',
-      "name": name,
-    }
-  }
+	async get_intrument_list(first_date, last_date, instrument_list, to_add_plot) {
+		if (!to_add_plot) {
+			await this.setStateAsync({ data: [] });
+		}
+		var i = 0;
+		for (i = 0; i < instrument_list.length; i++) {
+			await this.get_intrument_data(first_date, last_date, instrument_list[i], to_add_plot);
+		}
+	}
 
-  componentDidMount() {
+	setStateAsync(state) {
+		return new Promise((resolve) => {
+			this.setState(state, resolve);
+		});
+	}
 
-  }
+	async get_intrument_data(first_date, last_date, instrument_id) {
+		var i;
+		var x = [];
+		var y = [];
+		var name = 'zain';
 
-  get_intrument_list(first_date, last_date, instrument_list){
-    var i = 0
-    for (i = 0; i < instrument_list.length; i++) {
-      this.get_intrument_data(first_date, last_date, instrument_list[i], true)
-    }
-    
-  }
+		const requestOptions = {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0',
+			},
+		};
+		fetch(
+			'https://api.tase.co.il/api/ChartData/ChartData/?ct=1&ot=1&lang=1&cf=0&cp=4&cv=0&cl=0&cgt=1&dFrom=' +
+				first_date +
+				'&dTo=' +
+				last_date +
+				'&oid=' +
+				instrument_id,
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((jsonData) => {
+				// jsonData is parsed json object received from url
+				var points_for_chart = jsonData['PointsForHistoryChart'];
+				// console.log(points_for_chart)
+				// name = "zain"
+				var y_0 = points_for_chart[0]['ClosingRate'];
+				for (i = 0; i < points_for_chart.length; i++) {
+					var my_data = points_for_chart[i]['ClosingRate'] / y_0;
+					y.push(my_data);
+					x.push(i);
+				}
+			})
+			.catch((error) => {
+				// handle your errors here
+				console.error(error);
+			});
+		var temp_data = this.create_graph_data(x, y, name);
+		await this.setStateAsync({ data: [...this.state.data, temp_data] });
+	}
 
+	async componentDidUpdate(prevProps) {
+		// Typical usage (don't forget to compare props):
+		if (this.props.is_button_pressed !== prevProps.is_button_pressed) {
+			if (this.props.is_button_pressed) {
+				console.log('button presed in graph detected');
+				this.setState({ is_data_loaded: false });
+				var first_date = this.props.first_date;
+				var last_date = this.props.last_date;
+				var instrument_id = this.props.instrument_id;
+				var to_add_plot = this.props.to_add_plot;
+				//this.get_intrument_data(first_date,last_date,instrument_id, to_add_plot);
+				var temp_list = instrument_id;
 
-  get_intrument_data(first_date, last_date, instrument_id, to_add_plot){
-    var i;
-    var x = []
-    var y = []
-    var name = "zain"
-    
+				if (!Array.isArray(instrument_id)) {
+					temp_list = [instrument_id];
+				}
+				await this.get_intrument_list(first_date, last_date, temp_list, to_add_plot);
+				this.setState({ is_data_loaded: true });
+				this.props.graphHandler();
+				this.setState({ revision: this.state.revision + 1 });
+			}
+		}
+	}
 
-  const requestOptions = {
-    method: 'GET',
-    headers: {
-      "Accept"       : "application/json",
-      "Content-Type" : "application/json",
-      "User-Agent"   : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0" },
-};
-    console.log("firste_date, last_date, instrument_id",first_date,last_date, instrument_id  )
-    fetch("https://api.tase.co.il/api/ChartData/ChartData/?ct=1&ot=1&lang=1&cf=0&cp=4&cv=0&cl=0&cgt=1&dFrom="+first_date+"&dTo="+last_date+"&oid="+instrument_id, requestOptions).then(response => response.json())
-    .then((jsonData) => {
-    // jsonData is parsed json object received from url
-    var points_for_chart = jsonData["PointsForHistoryChart"]
-    console.log("get_intrument_data points_for_chart", points_for_chart)
-    // console.log(instrument_id)
-    // name = "zain"
-    var y_0 = points_for_chart[0]["ClosingRate"]
-    for (i = 0; i < points_for_chart.length; i++) {
-      var my_data = points_for_chart[i]["ClosingRate"] / y_0;
-      y.push(my_data)
-      x.push(i)
-    }
-    
-    })
-    .catch((error) => {
-    // handle your errors here
-    console.error(error)
-    })
-    var temp_data = this.create_graph_data(x,y, name)
-    
-    if (to_add_plot){
-      this.setState({ data: [...this.state.data, temp_data] })
-    }
-    else{
-      this.setState({ data: [temp_data] })
-    }
-    
-    this.setState({is_data_loaded : true})
-  }
-
-  componentDidUpdate(prevProps) {
-    // Typical usage (don't forget to compare props):
-    if (this.props.is_button_pressed !== prevProps.is_button_pressed) {
-      if (this.props.is_button_pressed){
-        this.setState({is_data_loaded : false})
-        var first_date = this.props.first_date
-        var last_date = this.props.last_date
-        var instrument_id = this.props.instrument_id
-        var to_add_plot = this.props.to_add_plot
-        //this.get_intrument_data(first_date,last_date,instrument_id, to_add_plot);
-        var temp_list = instrument_id
-        if (!Array.isArray(instrument_id)) {
-          temp_list = [instrument_id]
-        }
-        this.get_intrument_list(first_date,last_date,temp_list, to_add_plot)
-        this.props.graphHandler();
-  }
-    }
-
-  }
-
-  render() {
-
-  if ((this.state.is_data_loaded)){
-    return (
-      <Plot
-        data={this.state.data}
-        layout={{width: 800, height: 600, title: 'A Crazy Plot'}}
-      />
-    );
-      }
-      return (<h1>Loading...</h1>);
-  }
+	render() {
+		if (this.state.is_data_loaded) {
+			console.log('data is lodaded to render');
+			console.log(this.state.data);
+			return (
+				<Plot
+					data={this.state.data}
+					layout={{ width: 800, height: 600, title: 'A Crazy Plot', datarevision: 0 }}
+				/>
+			);
+		}
+		return <h1>Loading...</h1>;
+	}
 }
 export default Graph;
--- src/search_bar/Search.js
+++ src/search_bar/Search.js
@@ -1,87 +1,92 @@
 import React from 'react';
-import Graph from '../graph/Graph'
+import Graph from '../graph/Graph';
 import Information from '../info-json';
 
 class Search extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			search_keyword: 'תא 35',
+			is_data_loaded: false,
+			is_button_pressed: false,
+			to_add_plot: false,
+			result: [],
+			ids: [],
+			data: Information,
+		};
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.clearSearch = this.clearSearch.bind(this);
+		this.addSearch = this.addSearch.bind(this);
+		this.graphHandler = this.graphHandler.bind(this);
+	}
 
-  constructor(props) {
-      super(props)
-      this.state = { 
-        temp_search_keyword  :     "",
-        search_keyword       :     "",
-        is_data_loaded       :     false,
-        is_button_pressed    :     false,
-        to_add_plot          :     false,
-        result               :     [],
-        ids                  :     [],
-        data                 :     Information
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.clearSearch = this.clearSearch.bind(this);
-    this.addSearch = this.addSearch.bind(this);
-    this.graphHandler = this.graphHandler.bind(this)  
+	search() {
+		const filteredData = this.state.data.filter((item) => {
+			var res = Object.keys(item).some((key) => item[key].toLowerCase().includes(this.state.search_keyword));
+			return res;
+		});
+		this.setState({
+			result: filteredData,
+		});
+		var ids = [];
+		filteredData.forEach((item) => {
+			ids.push(item["מס' קרן"]);
+		});
+		this.setState({ ids: ids });
+	}
 
-  }
+	clearSearch() {
+		this.setState({ to_add_plot: false });
+		this.search();
+		this.setState({ is_button_pressed: true });
+	}
 
-    clearSearch() {  
-      this.setState({ search_keyword: this.state.temp_search_keyword });
-      this.setState({ is_button_pressed: true });
-  }
+	addSearch() {
+		console.log('addSearch: ', this.state.search_keyword);
+		this.setState({ to_add_plot: true });
+		this.search();
+		this.setState({ is_button_pressed: true });
+	}
 
-    addSearch() {  
-      console.log("addSearch: ", this.state.temp_search_keyword)
-      this.setState({ search_keyword: this.state.temp_search_keyword });
-      this.setState({ is_button_pressed: true });
-      this.setState({ to_add_plot: true });
-      const filteredData = this.state.data.filter(item => {
-          return Object.keys(item).some(key => item[key].toLowerCase().includes(this.state.search_keyword));
-      })
-      this.setState({
-          result:filteredData
-      })
-      var ids = []
-      filteredData.forEach((item) => {
-          ids.push(item["מס' קרן"])
-      });
-      this.setState({ids:ids})
-      console.log("ids:",ids)
-  }
+	handleInputChange(e) {
+		const content = e.target.value;
+		this.setState({ search_keyword: content });
+	}
 
-    handleInputChange(e) {
-      
-      const content = e.target.value;
-      console.log("handkeInputChange: content", content)
-      this.setState({ temp_search_keyword: content });
-    }
+	graphHandler() {
+		this.setState({ is_button_pressed: false });
+		this.setState({ to_add_plot: false });
+		console.log('search graph hanlder');
+		this.forceUpdate();
+	}
 
-    graphHandler() {
-      this.setState({ is_button_pressed: false})
-      this.setState({ to_add_plot: false });
-      setTimeout(function() { //Start the timer
-        this.setState({render: true}) //After 1 second, set render to true  //TODO: LAME
-    }.bind(this), 500)
-    }
+	render() {
+		var first_date = '23/04/2020';
+		var last_date = '23/09/2020';
+		console.log('render was called in search');
 
-    render() {
-
-      var first_date = "23/04/2020"
-      var last_date = "23/09/2020"
-
-      return (
-      <div>
-          <form>
-          <input
-            placeholder="Search for..."
-            value={this.state.query}
-            onChange={this.handleInputChange}
-          />
-        </form>
-        <button onClick={this.clearSearch}>new plot!</button>
-        <button onClick={this.addSearch}> add plot!</button>
-        <h1>Search</h1>
-        <Graph first_date={first_date} last_date={last_date} instrument_id={this.state.ids} graphHandler={this.graphHandler} is_button_pressed={this.state.is_button_pressed} to_add_plot={this.state.to_add_plot}/>
-      </div>
-      );
-    }
+		return (
+			<div>
+				<form>
+					<input
+						placeholder="Search for..."
+						value={this.state.search_keyword}
+						onChange={this.handleInputChange}
+					/>
+				</form>
+				<button onClick={this.clearSearch}>new plot!</button>
+				<button onClick={this.addSearch}> add plot!</button>
+				<h1>Search</h1>
+				<Graph
+					first_date={first_date}
+					last_date={last_date}
+					instrument_id={this.state.ids}
+					graphHandler={this.graphHandler}
+					is_button_pressed={this.state.is_button_pressed}
+					to_add_plot={this.state.to_add_plot}
+				/>
+			</div>
+		);
+	}
 }
 export default Search;
