2fceec4cbd08b6bbd8a4494a167b498715cb228d
nitsan57 <nitsan57@gmail.com>
2020-12-03 16:36:53 +0200

    [Graph] get data from bizportal and same length

--- src/graph/Graph.js
+++ src/graph/Graph.js
@@ -19,9 +19,6 @@ class Graph extends React.Component {
 	}
 
 	create_graph_data(x, y, name) {
-		// mode={'lines+markers'}
-		// marker={{ size: 10 }}
-		// line={{ shape: 'spline' }}
 		return {
 			x: x,
 			y: y,
@@ -34,13 +31,11 @@ class Graph extends React.Component {
 	componentDidMount() {}
 
 	async get_intrument_list(first_date, last_date, instrument_list, to_add_plot) {
+		var raw_data = [];
 		if (!to_add_plot) {
 			await this.setStateAsync({ data: [] });
 		}
-		var i = 0;
-		for (i = 0; i < instrument_list.length; i++) {
-			await this.get_intrument_data(first_date, last_date, instrument_list[i], to_add_plot);
-		}
+		await this.get_intruments_data(first_date, last_date, instrument_list, raw_data);
 	}
 
 	setStateAsync(state) {
@@ -49,43 +44,70 @@ class Graph extends React.Component {
 		});
 	}
 
-	async get_intrument_data(first_date, last_date, instrument) {
-		var i;
+	async get_graph_data(raw_data) {
+		// jsonData is parsed json object received from url
+
+		var points_for_chart = raw_data[0]['points'];
+		var min_data_length = points_for_chart.length;
+		var i = 0;
+		for (i = 0; i < raw_data.length; i++) {
+			points_for_chart = raw_data[i]['points'];
+			var d_length = points_for_chart[points_for_chart.length - 1].length;
+			if (min_data_length > d_length) {
+				min_data_length = d_length;
+			}
+		}
+		var name;
 		var x = [];
 		var y = [];
-		var name = instrument['name'];
-		var instrument_id = instrument['id'];
-		// console.log(instrument_id);
-		const requestOptions = {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-				'User-Agent':
-					'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Mobile Safari/537.36',
-			},
-		};
-		fetch(
-			'https://cors-anywhere.herokuapp.com/https://www.bizportal.co.il/forex/quote/ajaxrequests/paperdatagraphjson?period=fiveyearly&paperID=' +
-				instrument_id,
-			requestOptions
-		)
-			.then((response) => response.json())
-			.then((jsonData) => {
-				// jsonData is parsed json object received from url
-				var points_for_chart = jsonData['points'];
-				var y_0 = points_for_chart[points_for_chart.length - 1]['C_p'];
-				for (i = points_for_chart.length - 1; i > -1; i--) {
-					var my_data = points_for_chart[i]['C_p'] / y_0;
-					y.push(my_data);
-					x.push(points_for_chart[i]['D_p']);
-				}
-			})
-			.catch((error) => {
-				// handle your errors here
-				console.error(error);
-			});
-		var temp_data = this.create_graph_data(x, y, name);
-		await this.setStateAsync({ data: [...this.state.data, temp_data] });
+		var y_0;
+		var my_data;
+		var res = [];
+		var j = 0;
+		for (i = 0; i < raw_data.length; i++) {
+			name = raw_data[i]['paperName'];
+			points_for_chart = raw_data[i]['points'];
+			x = [];
+			y = [];
+			for (j = min_data_length - 1; j > -1; j--) {
+				y_0 = points_for_chart[min_data_length - 1]['C_p'];
+				my_data = points_for_chart[j]['C_p'] / y_0;
+				y.push(my_data);
+				x.push(points_for_chart[j]['D_p']);
+			}
+			var temp_data = this.create_graph_data(x, y, name);
+			res.push(temp_data);
+		}
+
+		this.setState({ data: res });
+	}
+
+	async get_intruments_data(first_date, last_date, instruments, raw_data) {
+		var instrument;
+		var i;
+		for (i = 0; i < instruments.length; i++) {
+			instrument = instruments[i];
+			var instrument_id = instrument['id'];
+			const requestOptions = {
+				method: 'GET',
+				headers: {
+					'User-Agent':
+						'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Mobile Safari/537.36',
+				},
+			};
+			try {
+				let response = await fetch(
+					'https://cors-anywhere.herokuapp.com/https://www.bizportal.co.il/forex/quote/ajaxrequests/paperdatagraphjson?period=fiveyearly&paperID=' +
+						instrument_id,
+					requestOptions
+				);
+				let jsonData = await response.json();
+				raw_data.push(jsonData);
+			} catch (err) {
+				console.log('Erorr in fetch');
+			}
+		}
+		await this.get_graph_data(raw_data);
 	}
 
 	async componentDidUpdate(prevProps) {
@@ -93,27 +115,15 @@ class Graph extends React.Component {
 		// Typical usage (don't forget to compare props):
 		if (this.props.is_button_pressed !== prevProps.is_button_pressed) {
 			if (this.props.is_button_pressed) {
-				// console.log('button presed in graph detected');
-				// console.log(this.props);
 				this.setState({ is_data_loaded: false });
 				var first_date = this.props.first_date;
 				var last_date = this.props.last_date;
 				var funds = this.props.funds;
 				var to_add_plot = this.props.to_add_plot;
-				//this.get_intrument_data(first_date,last_date,instrument_id, to_add_plot);
 
-				// if (!Array.isArray(instrument_id)) {
-				// 	temp_list = [instrument_id];
-				// }
 				await this.get_intrument_list(first_date, last_date, funds, to_add_plot);
 				this.props.graphHandler();
-				// this.setState({ datarevision: this.state.datarevision + 1 });
-				setTimeout(
-					function () {
-						this.setState({ is_data_loaded: true });
-					}.bind(this),
-					3000
-				);
+				this.setState({ is_data_loaded: true });
 			}
 		}
 	}
@@ -123,8 +133,7 @@ class Graph extends React.Component {
 			return <h1>Hi search something :)</h1>;
 		}
 		if (this.state.is_data_loaded) {
-			// console.log('data is lodaded to render');
-
+			// console.log(this.state.data);
 			return (
 				<Plot
 					data={this.state.data}
