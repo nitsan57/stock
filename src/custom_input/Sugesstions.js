4aca2fd4877c0f8c23961f4eb2e7be351ee613c4
nitsan57 <nitsan57@gmail.com>
2021-01-22 20:21:46 +0200

    [Suggestion] fix number of etfs to show

--- src/custom_input/Sugesstions.js
+++ src/custom_input/Sugesstions.js
@@ -6,10 +6,11 @@ const Suggestions = (props) => {
 	if (props.results.length === 0) {
 		return null;
 	}
-	let suggestions = props.results.slice(0, Consts.NUM_SEARCH_ELEMENTS_LIMIT_TO_SHOW);
-	// Consts.NUM_SEARCH_INDICES_LIMIT_TO_SHOW
-	suggestions.push({ id: 9999, name: props.text_lang.SUGGESTIONS.ALL });
+	let total_to_show = Consts.NUM_SEARCH_ELEMENTS_LIMIT_TO_SHOW + Consts.NUM_SEARCH_INDICES_LIMIT_TO_SHOW;
 	let show_indices = props.indices.slice(0, Consts.NUM_SEARCH_INDICES_LIMIT_TO_SHOW);
+	let indices_len = show_indices.length;
+	let suggestions = props.results.slice(0, total_to_show - indices_len);
+	suggestions.push({ id: 9999, name: props.text_lang.SUGGESTIONS.ALL });
 	const indices_list = show_indices.map((r, index) => (
 		<Button size="sm" key={index} variant="outline-secondary" onMouseDown={() => props.index_search(r)}>
 			{r}
